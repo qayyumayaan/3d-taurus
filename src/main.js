4addd0072d44b4340a276ca84baa512ca4549fc7
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-05-23 19:40:51 -0400

    Restore progress

--- src/main.js
+++ src/main.js
@@ -1,5 +1,54 @@
-import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// need a scene, camera, renderer
 
-createApp(App).mount('#app')
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .1, 1000 );
+
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render( scene, camera );
+
+
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 });
+const torus = new THREE.Mesh ( geometry, material );
+
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5)
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(pointLight, ambientLight)
+
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  torus.rotation.x += .01;
+  torus.rotation.y += .005;
+  torus.rotation.z += .01;
+  controls.update();
+
+
+  renderer.render( scene, camera );
+}
+
+animate()
+
